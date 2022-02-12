6122fedce67a4ae45fc1dab9847e2f535f3a007e
Hiren <hiren@freshbits.in>
2022-02-12 16:04:22 +0530

    Update migrations

--- database/migrations/2017_12_21_120348_create_carts_table.php
+++ database/migrations/2017_12_21_120348_create_carts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCartsTable extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -40,4 +39,4 @@ class CreateCartsTable extends Migration
     {
         Schema::dropIfExists('carts');
     }
-}
+};
